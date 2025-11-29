24b45300905c46c5c5c560b1cb9e544d35b19ce7
olzhassembinov <o_sembinov@kbtu.kz>
2025-11-29 10:56:19 +0500

    Author Commit

---
+++ StartApp/StartApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StartApp
+//
+//  Created by Олжас Сембинов on 29.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
