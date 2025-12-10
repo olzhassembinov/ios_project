704bcc0cd1b509ae9209939df335307baefe67d7
olzhassembinov <o_sembinov@kbtu.kz>
2025-12-10 21:27:58 +0500

    renew project

---
+++ StartHub/StartHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StartHub
+//
+//  Created by Олжас Сембинов on 10.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
