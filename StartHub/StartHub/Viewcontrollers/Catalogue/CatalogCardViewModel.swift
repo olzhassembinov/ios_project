9cacb9c8c02b56c4c6556d7108fabddde2c67fe0
Adlet Trum <adlettrum@MacBook-Air-Adlet.local>
2025-12-16 04:00:02 -0800

    catalog card view model

--- StartHub/StartHub/Viewcontrollers/Catalogue/CatalogCardViewModel.swift
+++ StartHub/StartHub/Viewcontrollers/Catalogue/CatalogCardViewModel.swift
@@ -5,3 +5,12 @@
 //  Created by Adlet Trum on 16.12.2025.
 //
 
+import Foundation
+
+struct CatalogCardViewModel {
+    let id: Int
+    let title: String
+    let description: String
+    let authorName: String
+    let imageURL: URL?
+}
